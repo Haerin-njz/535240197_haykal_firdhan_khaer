f2095c27f2e488ba66ba53a56771d52bcb8f41ec
Haykal_fk <haykal.535240197@stu.untar.ac.id>
2025-11-17 11:57:04 +0700

    teori tambahan

---
+++ app/api/groups/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import prisma from '../../lib/prisma';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    const newGroup = await prisma.kpopGroup.create({
+      data: {
+        name,
+      },
+    });
+    return NextResponse.json(newGroup);
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const groups = await prisma.kpopGroup.findMany();
+    return NextResponse.json(groups);
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
---
+++ app/lib/prisma/migrations/20251117044639_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "KpopGroup" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
